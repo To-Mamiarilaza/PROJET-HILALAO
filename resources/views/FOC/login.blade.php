b25216684c6a9f34d5e171c7922b1c4636f785b9
Vioart Vidoc Andriantsimiangy <vioartvidoc@gmail.com>
2023-06-06 21:06:01 +0300

    Add .env

--- resources/views/FOC/login.blade.php
+++ resources/views/FOC/login.blade.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('css/FOC/css/login.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/FOC/login.css') }}">
 	<title>HILALAO | LOGIN</title>
 </head>
 <body>
@@ -40,7 +40,7 @@
 			</form>
 				
 		</div>
-		<p>Don't have an account? <a href="{{ route('SignInAccount') }}">Sign up</a></p>
+		<p>Don't have an account? <a href="{{-- {{ route('SignInAccount') }} --}}">Sign up</a></p>
 	</div>
 </body>
 
--- routes/front_office_client_route.php
+++ routes/front_office_client_route.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\FOC\LoginController;
 use App\Http\Controllers\FOC\CustomerController;
 use App\Http\Controllers\FOC\InscriptionController;
 
+Route::get('/', function () {
+	return view('FOC/login');
+});
+
 Route::post('/SignIn', [LoginController::class, 'signin'])
 ->name('SignIn');
 
