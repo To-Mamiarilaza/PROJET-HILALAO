d100a1f185b2c458f32c849c2ae84aa115b0ad90
ahava <minoraherinirina72@gmail.com>
2023-06-06 22:01:06 +0200

    vue crud

--- resources/views/BO/crud.blade.php
+++ resources/views/BO/crud.blade.php
@@ -1,15 +1,60 @@
 
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>CRUD</title>
-    </head>
-    <body>
-    <h1>Liste des {{ $ref }} </h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/BO/asset/category.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Mon site</h1>
+    </header>
+
+    <nav>
         <ul>
-            @foreach ($all as $cat)
-                <li>{{ $cat->category }} {{ $cat->subscribing_price }}</li>
-            @endforeach
+            <li><a href="#">Statistique</a></li>
+            <li><a href="#">Abonnement</a></li>
+            <li><a href="#">Publicitaire</a></li>
+            <li><a href="#">Caracteristique</a></li>
         </ul>
-    </body>
+    </nav>
+
+    <form action="" method="post">
+        <h2>{{$ref}}</h2>
+        <h3>Listes des categories</h3>
+        <table border="1">
+            <tr>
+                <th>Nom</th>
+                <th>Prix</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+
+                <td>Categorie A</td>
+                <td>1000 Ar</td>
+                <td>
+                    <input type="button" value="Modifier">
+                    <input type="button" value="Supprimer">
+                </td>
+            </tr>
+            @foreach ($all as $crud)
+            <tr>
+                <td>{{ $crud->category }}</td>
+                <td>{{ $crud->subscribing_price }}</td>
+                <td>
+                    <input type="button" value="Modifier">
+                    <input type="button" value="Supprimer">
+                </td>
+            </tr>
+            @endforeach
+        </table>
+        <p>Ajouter une catégorie : <input type="text" name="category">
+        Ajouter le prix de l'abonnement : <input type="number" name="subscribing_price"> Ar</p>
+        <input type="submit" value="Ajouter">
+    </form>
+</body>
 </html>
+
