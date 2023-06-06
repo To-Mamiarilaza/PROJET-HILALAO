26648826acf3c9131f74663c448b1902e382b512
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2023-06-06 15:44:56 +0200

    Upload du script pour le base de donnée

---
+++ Information/data.sql
@@ -0,0 +1,243 @@
+CREATE SEQUENCE "public".abonnement_notification_id_abonnement_notification_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".abonnement_state_id_abonnement_state_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".account_admin_id_account_admin_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".category_id_category_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".cin_id_cin_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".client_id_client_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".day_id_day_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".direct_reservation_id_direct_reservation_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".discount2_id_discount_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".discount_id_discount_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".dispo_and_price_id_dispo_and_price_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".field_id_field_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".field_type_id_field_type_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".infrastruture_id_infrastructure_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".light_id_light_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".picture_id_picture_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".reservation_id_reservation_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".status_client_id_status_client_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".subscription2_id_subscription_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".type_picture_id_type_picture_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".unavailability_id_unavailability_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".user_reservation_notification_id_user_reservation_notificat_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".users_id_users_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "public".validation_notification_id_validation_notification_seq START WITH 1 INCREMENT BY 1;
+
+CREATE  TABLE "public".account_admin ( 
+	id_account_admin     integer DEFAULT nextval('account_admin_id_account_admin_seq'::regclass) NOT NULL  ,
+	first_name           varchar(50)    ,
+	last_name            varchar(50)  NOT NULL  ,
+	mail                 varchar(50)  NOT NULL  ,
+	phone_number         varchar(20)  NOT NULL  ,
+	pwd                  varchar(16)  NOT NULL  ,
+	CONSTRAINT pk_account_admin PRIMARY KEY ( id_account_admin )
+ );
+
+CREATE  TABLE "public".category ( 
+	id_category          integer DEFAULT nextval('category_id_category_seq'::regclass) NOT NULL  ,
+	category             varchar(50)  NOT NULL  ,
+	subscribing_price    numeric(10,2)  NOT NULL  ,
+	CONSTRAINT pk_category PRIMARY KEY ( id_category )
+ );
+
+CREATE  TABLE "public".cin ( 
+	id_cin               integer DEFAULT nextval('cin_id_cin_seq'::regclass) NOT NULL  ,
+	cin_number           varchar(20)    ,
+	first_picture        varchar(50)    ,
+	second_picture       varchar(50)    ,
+	CONSTRAINT pk_cin PRIMARY KEY ( id_cin )
+ );
+
+CREATE  TABLE "public"."day" ( 
+	id_day               integer DEFAULT nextval('day_id_day_seq'::regclass) NOT NULL  ,
+	"day"                varchar(35)  NOT NULL  ,
+	CONSTRAINT pk_tbl PRIMARY KEY ( id_day )
+ );
+
+CREATE  TABLE "public".field_type ( 
+	id_field_type        integer DEFAULT nextval('field_type_id_field_type_seq'::regclass) NOT NULL  ,
+	field_type           varchar(50)  NOT NULL  ,
+	CONSTRAINT pk_field_type PRIMARY KEY ( id_field_type )
+ );
+
+CREATE  TABLE "public".infrastructure ( 
+	id_infrastructure    integer DEFAULT nextval('infrastruture_id_infrastructure_seq'::regclass) NOT NULL  ,
+	infrastructure       varchar(50)  NOT NULL  ,
+	CONSTRAINT pk_infrastruture PRIMARY KEY ( id_infrastructure )
+ );
+
+CREATE  TABLE "public".light ( 
+	id_light             integer DEFAULT nextval('light_id_light_seq'::regclass) NOT NULL  ,
+	light                varchar(30)    ,
+	CONSTRAINT pk_light PRIMARY KEY ( id_light )
+ );
+
+CREATE  TABLE "public".status_client ( 
+	id_status_client     integer DEFAULT nextval('status_client_id_status_client_seq'::regclass) NOT NULL  ,
+	status               varchar(30)  NOT NULL  ,
+	CONSTRAINT pk_status PRIMARY KEY ( id_status_client )
+ );
+
+CREATE  TABLE "public".subscription_state ( 
+	id_subscription_state integer DEFAULT nextval('abonnement_state_id_abonnement_state_seq'::regclass) NOT NULL  ,
+	subscription_state   varchar(20)  NOT NULL  ,
+	CONSTRAINT pk_abonnement_state PRIMARY KEY ( id_subscription_state )
+ );
+
+CREATE  TABLE "public".type_picture ( 
+	id_type_picture      integer DEFAULT nextval('type_picture_id_type_picture_seq'::regclass) NOT NULL  ,
+	type_picture         varchar(50)  NOT NULL  ,
+	CONSTRAINT pk_type_picture PRIMARY KEY ( id_type_picture )
+ );
+
+CREATE  TABLE "public".users ( 
+	id_users             integer DEFAULT nextval('users_id_users_seq'::regclass) NOT NULL  ,
+	first_name           varchar(30)  NOT NULL  ,
+	last_name            varchar(50)  NOT NULL  ,
+	birth_date           date  NOT NULL  ,
+	phone_number         varchar(20)  NOT NULL  ,
+	mail                 varchar(50)  NOT NULL  ,
+	sign_up_date         date DEFAULT CURRENT_DATE NOT NULL  ,
+	pwd                  varchar(16)  NOT NULL  ,
+	CONSTRAINT pk_users PRIMARY KEY ( id_users )
+ );
+
+CREATE  TABLE "public".client ( 
+	id_client            integer DEFAULT nextval('client_id_client_seq'::regclass) NOT NULL  ,
+	first_name           varchar(30)  NOT NULL  ,
+	last_name            varchar(30)  NOT NULL  ,
+	phone_number         varchar(20)  NOT NULL  ,
+	mail                 varchar(50)  NOT NULL  ,
+	address              varchar(50)  NOT NULL  ,
+	birth_date           date  NOT NULL  ,
+	pwd                  varchar(16)  NOT NULL  ,
+	id_status            integer  NOT NULL  ,
+	sign_up_date         timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL  ,
+	id_cin               integer    ,
+	CONSTRAINT pk_client PRIMARY KEY ( id_client ),
+	CONSTRAINT fk_client_cin FOREIGN KEY ( id_cin ) REFERENCES "public".cin( id_cin )   ,
+	CONSTRAINT fk_client_status_client FOREIGN KEY ( id_status ) REFERENCES "public".status_client( id_status_client )   
+ );
+
+CREATE  TABLE "public".field ( 
+	id_field             integer DEFAULT nextval('field_id_field_seq'::regclass) NOT NULL  ,
+	id_category          integer  NOT NULL  ,
+	id_client            integer  NOT NULL  ,
+	name                 varchar(100)  NOT NULL  ,
+	id_field_type        integer  NOT NULL  ,
+	id_infrastructure    integer  NOT NULL  ,
+	id_light             integer  NOT NULL  ,
+	description          text  NOT NULL  ,
+	address              varchar(200)    ,
+	latitude             double precision  NOT NULL  ,
+	longitude            double precision  NOT NULL  ,
+	insert_date          date DEFAULT CURRENT_DATE   ,
+	field_files          varchar(100)  NOT NULL  ,
+	CONSTRAINT pk_field PRIMARY KEY ( id_field ),
+	CONSTRAINT fk_field_category FOREIGN KEY ( id_category ) REFERENCES "public".category( id_category )   ,
+	CONSTRAINT fk_field_client FOREIGN KEY ( id_client ) REFERENCES "public".client( id_client )   ,
+	CONSTRAINT fk_field_field_type FOREIGN KEY ( id_field_type ) REFERENCES "public".field_type( id_field_type )   ,
+	CONSTRAINT fk_field_infrastruture FOREIGN KEY ( id_infrastructure ) REFERENCES "public".infrastructure( id_infrastructure )   ,
+	CONSTRAINT fk_field_light FOREIGN KEY ( id_light ) REFERENCES "public".light( id_light )   
+ );
+
+CREATE  TABLE "public".picture ( 
+	id_picture           integer DEFAULT nextval('picture_id_picture_seq'::regclass) NOT NULL  ,
+	picture              varchar(50)  NOT NULL  ,
+	id_type_picture      integer  NOT NULL  ,
+	id_field             integer  NOT NULL  ,
+	CONSTRAINT pk_pictures PRIMARY KEY ( id_picture ),
+	CONSTRAINT fk_picture_field FOREIGN KEY ( id_field ) REFERENCES "public".field( id_field )   ,
+	CONSTRAINT fk_picture_type_picture FOREIGN KEY ( id_type_picture ) REFERENCES "public".type_picture( id_type_picture )   
+ );
+
+CREATE  TABLE "public".reservation ( 
+	id_reservation       integer DEFAULT nextval('reservation_id_reservation_seq'::regclass) NOT NULL  ,
+	reservation_date     date  NOT NULL  ,
+	id_users             integer  NOT NULL  ,
+	start_time           time  NOT NULL  ,
+	id_field             integer  NOT NULL  ,
+	duration             integer  NOT NULL  ,
+	CONSTRAINT pk_reservation PRIMARY KEY ( id_reservation ),
+	CONSTRAINT fk_reservation_field FOREIGN KEY ( id_field ) REFERENCES "public".field( id_field )   ,
+	CONSTRAINT fk_reservation_users FOREIGN KEY ( id_users ) REFERENCES "public".users( id_users )   
+ );
+
+CREATE  TABLE "public".subscription ( 
+	id_subscription      integer DEFAULT nextval('subscription2_id_subscription_seq'::regclass) NOT NULL  ,
+	id_field             integer  NOT NULL  ,
+	subscription_date    date DEFAULT CURRENT_DATE NOT NULL  ,
+	start_date           date  NOT NULL  ,
+	duration             integer  NOT NULL  ,
+	id_subscription_state integer  NOT NULL  ,
+	CONSTRAINT pk_subscription2 PRIMARY KEY ( id_subscription ),
+	CONSTRAINT fk_subscription2_field FOREIGN KEY ( id_field ) REFERENCES "public".field( id_field )   ,
+	CONSTRAINT fk_subscription2_subscription_state FOREIGN KEY ( id_subscription_state ) REFERENCES "public".subscription_state( id_subscription_state )   
+ );
+
+CREATE  TABLE "public".unavailability ( 
+	id_unavailability    integer DEFAULT nextval('unavailability_id_unavailability_seq'::regclass) NOT NULL  ,
+	id_field             integer  NOT NULL  ,
+	unavailability_date  date  NOT NULL  ,
+	start_time           time  NOT NULL  ,
+	end_time             time  NOT NULL  ,
+	CONSTRAINT pk_unavailability PRIMARY KEY ( id_unavailability ),
+	CONSTRAINT fk_unavailability_field FOREIGN KEY ( id_field ) REFERENCES "public".field( id_field )   
+ );
+
+CREATE  TABLE "public".direct_reservation ( 
+	id_direct_reservation integer DEFAULT nextval('direct_reservation_id_direct_reservation_seq'::regclass) NOT NULL  ,
+	client_name          varchar(100)    ,
+	phone_number_client  varchar(20)    ,
+	reservation_date     date DEFAULT CURRENT_DATE   ,
+	start_time           time  NOT NULL  ,
+	id_field             integer  NOT NULL  ,
+	duration             integer  NOT NULL  ,
+	CONSTRAINT pk_direct_reservation PRIMARY KEY ( id_direct_reservation ),
+	CONSTRAINT fk_direct_reservation_field FOREIGN KEY ( id_field ) REFERENCES "public".field( id_field )   
+ );
+
+CREATE  TABLE "public".discount ( 
+	id_discount          integer DEFAULT nextval('discount2_id_discount_seq'::regclass) NOT NULL  ,
+	id_field             integer  NOT NULL  ,
+	"start"              date  NOT NULL  ,
+	"end"                date  NOT NULL  ,
+	discount             numeric(5,2)  NOT NULL  ,
+	CONSTRAINT pk_discount2 PRIMARY KEY ( id_discount ),
+	CONSTRAINT fk_discount2_field FOREIGN KEY ( id_field ) REFERENCES "public".field( id_field )   
+ );
+
+CREATE  TABLE "public".dispo_and_price ( 
+	id_dispo_and_price   integer DEFAULT nextval('dispo_and_price_id_dispo_and_price_seq'::regclass) NOT NULL  ,
+	id_day               integer  NOT NULL  ,
+	start_time           time  NOT NULL  ,
+	end_time             time  NOT NULL  ,
+	id_field             integer  NOT NULL  ,
+	price                numeric(10,2)  NOT NULL  ,
+	CONSTRAINT pk_dispo_and_price PRIMARY KEY ( id_dispo_and_price ),
+	CONSTRAINT fk_dispo_and_price_day FOREIGN KEY ( id_day ) REFERENCES "public"."day"( id_day )   ,
+	CONSTRAINT fk_dispo_and_price_field FOREIGN KEY ( id_field ) REFERENCES "public".field( id_field )   
+ );
---
+++ Information/données_de_test.sql
@@ -0,0 +1,105 @@
+
+-- Données pour le client
+INSERT INTO status_client VALUES
+(DEFAULT, 'Confirmé'),
+(DEFAULT, 'En attente');
+
+INSERT INTO cin VALUES 
+(DEFAULT, '117 231 421 552', 'client1_cin_picture1.png', 'client1_cin_picture2.png'),
+(DEFAULT, '224 005 317 251', 'client2_cin_picture1.png', 'client2_cin_picture2.png'),
+(DEFAULT, '119 105 250 300', 'client3_cin_picture1.png', 'client3_cin_picture2.png');
+
+INSERT INTO client VALUES
+(DEFAULT, 'John', 'RAKOTOARISOA', '0321531728', 'john.rakoto@gmail.com', 'Antananarivo', '2000-11-24', 'john1234', 1, '2023-01-15', 1),
+(DEFAULT, 'Andry', 'ANDRIAMAMITIANA', '0341451743', 'andry123@gmail.com', 'Lot 3 bis Iavoloha', '1987-01-12', 'andry-andry', 1, '2023-04-23', 2),
+(DEFAULT, 'Fitia', 'MIAROTIA', '0331754393', 'fitiaMia@gmail.com', 'Lot IAV 416', '1998-07-05', 'fitia@@', 1, '2023-10-07', 3);
+
+-- Données pour les informations requis pour le terrain
+INSERT INTO category VALUES 
+(DEFAULT, 'Foot à 11', 75000),
+(DEFAULT, 'Basket', 20000),
+(DEFAULT, 'Foot à 7', 50000);
+
+INSERT INTO field_type VALUES 
+(DEFAULT, 'Goudron'),
+(DEFAULT, 'Synthétique'),
+(DEFAULT, 'Gazon'),
+(DEFAULT, 'Terre'),
+(DEFAULT, 'Tapis'),
+(DEFAULT, 'Parquet');
+
+INSERT INTO infrastructure VALUES
+(DEFAULT, 'Intérieur'),
+(DEFAULT, 'Extérieur');
+
+INSERT INTO light VALUES
+(DEFAULT, 'Eclairé'),
+(DEFAULT, 'Non Eclairé');
+
+INSERT INTO day VALUES
+(DEFAULT, 'Lundi'),
+(DEFAULT, 'Mardi'),
+(DEFAULT, 'Mercredi'),
+(DEFAULT, 'Jeudi'),
+(DEFAULT, 'Vendredi'),
+(DEFAULT, 'Samedi'),
+(DEFAULT, 'Dimanche');
+
+-- Insertion donnee de test terrain
+insert into type_picture values
+	(DEFAULT, 'Profil'),
+	(DEFAULT, 'Secondaire');
+
+insert into field values
+-- 	(id_field,	id_category,	id_client,	name,						id_field_type,	id_infrastructure,	id_light,	description,									address,					latitude,	longitude,	insert_date,	field_files)
+	(DEFAULT,	2,				1,			'Basket Betongolo',			1,				2,					2,			'Terrain fait pour tous et libre a tous',		'Betongolo Antananarivo',	-18.904924,	47.541774,	'2023-02-20',	'Field_Files/1/Proprio.zip'),
+	(DEFAULT,	2,				2,			'Terrain Antsahamasina',	1,				2,					2,			'Terrain approprier au entrainement de club',	'3FW9+J8M, Antananarivo',	-18.903418,	47.468301,	'2023-01-14',	'Field_Files/2/Proprio.zip'),
+	(DEFAULT,	3,				2,			'Foot Elgeco plus',			1,				2,					2,			'Terrain synthetique et bien eclaire pour vous','3FW9+J8M, Antananarivo',	-18.904924,	47.541774,	'2023-01-09',	'Field_Files/3/Proprio.zip'),
+	(DEFAULT,	1,				3,			'Foot Domaine Alasora',		1,				2,					2,			'Terrain approprier au entrainement de club',	'3FW9+J8M, Antananarivo',	-18.904924,	47.541774,	'2023-03-27',	'Field_Files/4/Proprio.zip');
+
+insert into picture values
+-- 	(id_picture,	picture,			id_type_picture,	id_field)
+	(DEFAULT,		'Terrain1.jpg',		1,					1),
+	(DEFAULT,		'Terrain2.jpg',		1,					2),
+	(DEFAULT,		'Terrain3.jpg',		2,					3),
+	(DEFAULT,		'Terrain4.jpg',		2,					4);
+
+INSERT INTO dispo_and_price VALUES 
+(DEFAULT, 1, '08:00:00', '19:00:00', 1, 30000),
+(DEFAULT, 2, '08:00:00', '19:00:00', 1, 30000),
+(DEFAULT, 3, '08:00:00', '19:00:00', 1, 30000),
+(DEFAULT, 4, '08:00:00', '19:00:00', 1, 30000),
+(DEFAULT, 5, '08:00:00', '19:00:00', 1, 30000),
+(DEFAULT, 3, '12:00:00', '19:30:00', 2, 28000),
+(DEFAULT, 5, '08:00:00', '19:00:00', 2, 28000),
+(DEFAULT, 6, '08:00:00', '20:00:00', 2, 30000),
+(DEFAULT, 7, '14:00:00', '18:00:00', 2, 35000),
+(DEFAULT, 1, '09:00:00', '12:00:00', 3, 50000),
+(DEFAULT, 2, '08:00:00', '18:00:00', 3, 50000),
+(DEFAULT, 3, '08:00:00', '12:00:00', 3, 50000),
+(DEFAULT, 3, '12:00:00', '20:00:00', 3, 65000),
+(DEFAULT, 4, '08:00:00', '19:00:00', 3, 60000),
+(DEFAULT, 5, '08:00:00', '18:00:00', 3, 60000),
+(DEFAULT, 5, '18:00:01', '22:00:00', 3, 80000),
+(DEFAULT, 6, '05:00:00', '18:00:00', 3, 60000),
+(DEFAULT, 6, '18:00:01', '22:00:00', 3, 65000),
+(DEFAULT, 7, '14:00:00', '18:00:00', 3, 60000),
+(DEFAULT, 1, '08:00:00', '18:00:00', 4, 40000),
+(DEFAULT, 2, '08:00:00', '18:00:00', 4, 40000),
+(DEFAULT, 3, '08:00:00', '18:00:00', 4, 40000),
+(DEFAULT, 5, '08:00:00', '19:00:00', 4, 50000),
+(DEFAULT, 6, '05:00:00', '20:00:00', 4, 50000);
+
+INSERT INTO discount VALUES 
+(DEFAULT, 1, '2023-06-06', '2023-06-10', 20),
+(DEFAULT, 4, '2023-06-16', '2023-06-20', 10);
+
+INSERT INTO unavailability VALUES
+(DEFAULT, 1, '2023-06-26', '08:00:00', '19:00:00'),
+(DEFAULT, 3, '2023-06-25', '12:00:00', '20:00:00'),
+(DEFAULT, 3, '2023-06-26', '05:00:00', '20:00:00');
+
+INSERT INTO direct_reservation VALUES
+(DEFAULT, 'Feno', '0331854735', '2023-06-12', '08:00:00', 1, 1),
+(DEFAULT, 'Zoky andry', '0328254518', '2023-06-10', '18:30:00', 1, 3);
+
