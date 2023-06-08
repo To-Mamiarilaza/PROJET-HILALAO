a9307650e846b085d9237672cd81a1b8a80192db
Fanambony <ramarosonbenjamina@gmail.com>
2023-06-08 18:58:33 +0300

    nav

--- resources/views/BO/nav.blade.php
+++ resources/views/BO/nav.blade.php
@@ -1,15 +1,32 @@
 <nav>
-    <ul>
-        <li><a href="#">Statistique</a></li>
-        <li><a href="#">Abonnement</a></li>
-        <li><a href="#">Publicitaire</a></li>
-        <li>
-            <ul><a href="#">Caracteristique</a>
-                <li><a href="{{ route('crud', ['variable' => 'category']) }}"> Category</a> </li>
-                <li><a href="{{ route('crud', ['variable' => 'subscription_state']) }}"> Subscription_state</a> </li>
-                <li><a href="{{ route('crud', ['variable' => 'field_type']) }}"> Field Type</a> </li>
-            </ul>
 
-        </li>
+    <div>
+        
+    </div>
+
+    <p><a href="#">Statistique</a></p>
+    <p><a href="#">Abonnement</a></p>
+    <p><a href="#">Publicitaire</a></p>
+
+    <a href="#" id="toggleList">Caracteristique x</a>
+    <ul id="maListe" style="display: none">
+        <li><a href="{{ route('crud', ['variable' => 'category']) }}"> Category</a></li>
+        <li><a href="{{ route('crud', ['variable' => 'subscription_state']) }}"> Subscription state</a></li>
+        <li><a href="{{ route('crud', ['variable' => 'field_type']) }}"> Field Type</a></li>
     </ul>
+
+    <script>
+        document.getElementById('toggleList').addEventListener('click', function() {
+            var liste = document.getElementById('maListe');
+            if(liste.style.display === 'none') {
+                liste.style.display = 'block';
+            } else {
+                liste.style.display = 'none';
+            }
+        });
+    </script>
+
+    {{-- <p><a href="{{ route('crud', ['variable' => 'category']) }}"> Category</a></p>
+    <p><a href="{{ route('crud', ['variable' => 'subscription_state']) }}"> Subscription_state</a></p>
+    <p><a href="{{ route('crud', ['variable' => 'field_type']) }}"> Field Type</a></p> --}}
 </nav>
