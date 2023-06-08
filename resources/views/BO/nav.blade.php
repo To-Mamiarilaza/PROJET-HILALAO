cd7c7b19462dc6d15e25e2de7326dff6d0898c27
ahava <minoraherinirina72@gmail.com>
2023-06-08 18:07:55 +0200

    nav caracteristique

--- resources/views/BO/nav.blade.php
+++ resources/views/BO/nav.blade.php
@@ -3,13 +3,21 @@
         <li><a href="{{ route('statistique') }}">Statistique</a></li>
         <li><a href="#">Abonnement</a></li>
         <li><a href="#">Publicitaire</a></li>
-        <li>
-            <ul><a href="#">Caracteristique</a>
+        <li> <a href="#" id="toggleList">Caracteristique</a></li>
+            <ul id="maListe" style="display: none;">
                 <li><a href="{{ route('crud', ['variable' => 'category']) }}"> Category</a> </li>
                 <li><a href="{{ route('crud', ['variable' => 'subscription_state']) }}"> Subscription_state</a> </li>
                 <li><a href="{{ route('crud', ['variable' => 'field_type']) }}"> Field Type</a> </li>
             </ul>
-
-        </li>
     </ul>
+    <script>
+        document.getElementById('toggleList').addEventListener('click', function() {
+        var liste = document.getElementById('maListe');
+        if (liste.style.display === 'none') {
+            liste.style.display = 'block';
+        } else {
+            liste.style.display = 'none';
+        }
+        });
+    </script>
 </nav>
