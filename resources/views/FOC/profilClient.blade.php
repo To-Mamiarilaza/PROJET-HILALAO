dfca5a49a9cc008d01c08779f2aa27e494604cc3
Vioart Vidoc Andriantsimiangy <vioartvidoc@gmail.com>
2023-06-08 17:15:06 +0300

    Update

--- resources/views/FOC/profilClient.blade.php
+++ resources/views/FOC/profilClient.blade.php
@@ -12,52 +12,50 @@
 <body>
 	<div class="container box">
 		<div class="row justify-content-center">
+			<h1 class="box__title">Profil <span class="box__title--span">Client</span></h1>
 			<div class="row">
-				<div class="card mt-5">
-					<h1 class="box__title">Profil <span class="box__title--span">Client</span></h1>
-					<div class="row">
-						<div class="col-md-3 input_pictures profil">
-							<h5 class="card-title">Picture Profil</h5>
-							<input type="file" id="image-upload" style="display: none;">
-							<label for="image-upload" class="btn btn-primary btn-block">Upload picture</label>
-						</div>
-						<div class="col-md-3 cinPdp">
-							<h5 class="card-title">CIN recto</h5>
-							<a href="#" class="thumbnail">
-								<img src="..." alt="...">
-							</a>
-						</div>
-						<div class="col-md-3 cinPdp">
-							<h5 class="card-title">CIN verso</h5>
-							<a href="#" class="thumbnail">
-								<img src="..." alt="...">
-							</a>
-						</div>
-					</div>
-					<div class="form-group infoProfil">
-						<label for="nom">First Name :</label>
-						<input type="text" id="nom" class="form-control" value="John" disabled>
-					</div>
-					<div class="form-group infoProfil">
-						<label for="tel">Last Name :</label>
-						<input type="text" class="form-control" name="tel" id="tel" value="Doe" disabled>
-					</div>
-					<div class="form-group infoProfil">
-						<label for="tel">Phone number :</label>
-						<input type="number" class="form-control" name="tel" id="tel" value="0345510234" disabled>
-					</div>
-					<div class="form-group infoProfil">
-						<label for="email">Email :</label>
-						<input type="email" id="email" class="form-control" value="johndoe@example.com" disabled>
-					</div>
-					<div class="form-group infoProfil">
-						<label for="adresse">Adresse :</label>
-						<input type="text" class="form-control" name="adresse" id="adresse" value="123 Rue du Client, Ville, Pays" disabled>
-					</div>
-					<div class="form-group infoProfil">
-						<label for="adresse">CIN number :</label>
-						<input type="number" class="form-control" name="adresse" id="adresse" value="101348477166" disabled>
-					</div>
+				<div class="col-md-3 input_pictures profil">
+					<h5 class="card-title">Picture Profil</h5>
+					<input type="file" id="image-upload" style="display: none;">
+					<label for="image-upload" class="btn btn-primary btn-block">Upload picture</label>
+				</div>
+				<div class="col-md-3 cinPdp">
+					<h5 class="card-title">CIN recto</h5>
+					<a href="#" class="thumbnail">
+						<img src="..." alt="...">
+					</a>
+				</div>
+				<div class="col-md-3 cinPdp">
+					<h5 class="card-title">CIN verso</h5>
+					<a href="#" class="thumbnail">
+						<img src="..." alt="...">
+					</a>
+				</div>
+			</div>
+			<div class="card mt-5">
+				<div class="form-group infoProfil">
+					<label for="nom">First Name :</label>
+					<input type="text" id="nom" class="form-control" value="John" disabled>
+				</div>
+				<div class="form-group infoProfil">
+					<label for="tel">Last Name :</label>
+					<input type="text" class="form-control" name="tel" id="tel" value="Doe" disabled>
+				</div>
+				<div class="form-group infoProfil">
+					<label for="tel">Phone number :</label>
+					<input type="number" class="form-control" name="tel" id="tel" value="0345510234" disabled>
+				</div>
+				<div class="form-group infoProfil">
+					<label for="email">Email :</label>
+					<input type="email" id="email" class="form-control" value="johndoe@example.com" disabled>
+				</div>
+				<div class="form-group infoProfil">
+					<label for="adresse">Adresse :</label>
+					<input type="text" class="form-control" name="adresse" id="adresse" value="123 Rue du Client, Ville, Pays" disabled>
+				</div>
+				<div class="form-group infoProfil">
+					<label for="adresse">CIN number :</label>
+					<input type="number" class="form-control" name="adresse" id="adresse" value="101348477166" disabled>
 				</div>
 			</div>
 		</div>
