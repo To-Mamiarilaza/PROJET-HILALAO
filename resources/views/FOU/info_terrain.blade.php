e1ca989bf554d7e62e05552e037a209463ffd6e9
EricMahafaly <120698551+EricMahafaly@users.noreply.github.com>
2023-06-10 01:02:55 +0200

    Update info_terrain.blade.php

--- resources/views/FOU/info_terrain.blade.php
+++ resources/views/FOU/info_terrain.blade.php
@@ -1,11 +1,37 @@
-@foreach($infos as $info)
-    <p>{{$info->name}}</p>
-    <p>{{$info->category}}</p>
-    <p>{{$info->field_type}}</p>
-    <p>{{$info->infrastructure}}</p>
-    <p>{{$info->light}}</p>
-    <p>{{$info->address}}</p>
-    <p>{{$info->longitude}}</p>
-    <p>{{$info->latitude}}</p>
-    <p>{{$info->description}}</p>
-@endforeach
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/FOU/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/FOU/assets/css/list-terrain.css') }} ">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Information Terrain</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Information du terrain</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                @foreach($infos as $info)
+                    <p>Nom du terrain : {{$info->name}}</p>
+                    <p>Catégorie : {{$info->category}}</p>
+                    <p>Type de terrain : {{$info->field_type}}</p>
+                    <p>infrastructure : {{$info->infrastructure}}</p>
+                    <p>L'intensité de la lumière : {{$info->light}}</p>
+                    <p>Adresse : {{$info->address}}</p>
+                    <p>Coordonnée Géographique : longitude : {{$info->longitude}} et latitude : {{$info->latitude}}</p>
+                    <p>Description : {{$info->description}}</p>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+
