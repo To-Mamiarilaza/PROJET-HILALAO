15a0d3924a937239f8adc5f98e1d0bd39211d36f
Vioart Vidoc Andriantsimiangy <vioartvidoc@gmail.com>
2023-06-13 08:22:48 +0300

    Update addTerrain-Dispo.blade.php

---
+++ resources/views/FOC/addTerrain-Dispo.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('fontawesome-5/css/all.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/FOC/home.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/FOC/add-field.css') }}">
+	<title>HILALAO | TERRAIN - DISPONIBILITE & PRIX</title>
+</head>
+<body>
+	@include('FOC/header');
+
+	<section></section>
+</body>
+</html>
