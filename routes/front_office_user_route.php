10b98ff6d49a515e5628591743826a1305827707
EricMahafaly <120698551+EricMahafaly@users.noreply.github.com>
2023-06-16 19:27:54 +0200

    carte

--- routes/front_office_user_route.php
+++ routes/front_office_user_route.php
@@ -1,8 +1,11 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FOU\MyListFieldController;
+use App\Http\Controllers\FOU\InfoTerrain;
 
 Route::get('/ListFields', [MyListFieldController::class, 'index']);
 
 
-Route::get('/infoTerrain/{id_field}', [\App\Http\Controllers\FOU\InfoTerrain::class, 'index']);
+Route::get('/infoTerrain/{id_field}', [InfoTerrain::class, 'index']);
+
+Route::get('/carte', [InfoTerrain::class, 'afficheCarte']);
