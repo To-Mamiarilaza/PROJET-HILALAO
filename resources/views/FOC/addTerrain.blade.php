7411b5a04fc5595e276e8570511a5d392616d6c3
Vioart Vidoc Andriantsimiangy <vioartvidoc@gmail.com>
2023-06-20 17:53:09 +0300

    aa

--- resources/views/FOC/addTerrain.blade.php
+++ resources/views/FOC/addTerrain.blade.php
@@ -20,7 +20,7 @@
 					<img src="{{ asset('image/add-terrain.png') }}" alt="Image terrain" srcset="">
 				</div>
 				<div class="col-md-6">
-					<h1>Ajouter vos terrains dans la liste</h1>
+					{{-- <h1>Ajouter vos terrains dans la liste</h1> --}}
 					<p class="mt-4">
 						Vous pouvez bien sûr ajouter votre terrain ainsi que d'autres informations nécessaires 
 						afin que les utilisateurs puissent mieux voir les merveilles sur le terrain où ils jouent.
@@ -31,81 +31,94 @@
 			<div class="row justify-content-center fonction-list">
                 <h1>Formulaire d'ajout de terrain</h1>
 				<div class="col-md-12 contenu-terrain">
-					<form action="" method="get" class="col-md-12 form-content">
-						<div class="row info-terrain">
-							<div class="col-md-2">
-								<label for="nom" class="control-label"><b>Nom</b></label>
-							</div>
-							<div class="col-md-8">
-								<input class="form-content__input form-content__input--log form-control" type="text" placeholder="exemple@gmail.com" aria-label=".form-control-lg" id="nom" name="nom" placeholder="nom du terrain">
-							</div>
-						</div>
-						<div class="row info-terrain">
-							<div class="col-md-2">
-								<label for="categories" class="control-label"><b>Catégories</b></label>
-							</div>
-							<div class="col-md-8">
-								<select id="categories" class="form-control" required>
-									<option value="">Sélectionnez une catégorie</option>
-									<option value="football">Foot à 11</option>
-									<option value="basketball">Foot à 7</option>
-									<option value="tennis">Basket</option>
-								</select>
-							</div>
-						</div>
-						<div class="row info-terrain">
-							<div class="col-md-2">
-								<label for="surface" class="control-label"><b>Surface</b></label>
-							</div>
-							<div class="col-md-8">
-								<select id="surface" class="form-control" required>
-									<option value="">Sélectionnez une type de surface</option>
-									<option value="Goudron">Goudron</option>
-									<option value="Synthétique">Synthétique</option>
-									<option value="Gazon">Gazon</option>
-									<option value="Terre">Terre</option>
-									<option value="Tapis">Tapis</option>
-									<option value="Parquet">Parquet</option>
-								</select>
-							</div>
-						</div>
-						<div class="row info-terrain">
-							<div class="col-md-2">
-								<label for="infrastructure" class="cotrol-label"><b>Infrastructure</b></label>
-							</div>
-							<div class="col-md-8">
-								<div class="radio">
-									<label>
-										<input type="radio" name="optionsRadios" id="" value="">
-										Intérieur
-									</label>
-									<label>
-										<input type="radio" name="optionsRadios" id="" value="">
-										Extérieur
-									</label>
+					<div class="col-md-8">
+						<form action="" method="get" class="form-content">
+							<div class="row info-terrain">
+								<div class="col-md-2">
+									<label for="nom" class="control-label"><b>Nom</b></label>
+								</div>
+								<div class="col-md-6">
+									<input class="form-content__input form-content__input--log form-control" type="text" aria-label=".form-control-lg" id="nom" name="nom" placeholder="nom du terrain">
 								</div>
 							</div>
-						</div>
-						<div class="row info-terrain">
-							<div class="col-md-2">
-								<label for="eclairage" class="control-label"><b>Eclairage</b></label>
-							</div>
-							<div class="col-md-8">
-								<div class="radio">
-									<label>
-										<input type="radio" name="optionsRadios" id="" value="">
-										Eclairé
-									</label>
-									<label>
-										<input type="radio" name="optionsRadios" id="" value="">
-										Non Eclairé
-									</label>
+							<div class="row info-terrain">
+								<div class="col-md-2">
+									<label for="categories" class="control-label"><b>Catégories</b></label>
+								</div>
+								<div class="col-md-6">
+									<select id="categories" class="form-control" required>
+										<option value="">Sélectionnez une catégorie</option>
+										<option value="football">Foot à 11</option>
+										<option value="basketball">Foot à 7</option>
+										<option value="tennis">Basket</option>
+									</select>
 								</div>
 							</div>
-						</div>
-						<input class="btn btn-success" type="submit" value="Ajouter">
+							<div class="row info-terrain">
+								<div class="col-md-2">
+									<label for="surface" class="control-label"><b>Surface</b></label>
+								</div>
+								<div class="col-md-6">
+									<select id="surface" class="form-control" required>
+										<option value="">Sélectionnez une type de surface</option>
+										<option value="Goudron">Goudron</option>
+										<option value="Synthétique">Synthétique</option>
+										<option value="Gazon">Gazon</option>
+										<option value="Terre">Terre</option>
+										<option value="Tapis">Tapis</option>
+										<option value="Parquet">Parquet</option>
+									</select>
+								</div>
+							</div>
+							<div class="row info-terrain">
+								<div class="col-md-2">
+									<label for="infrastructure" class="cotrol-label"><b>Infrastructure</b></label>
+								</div>
+								<div class="col-md-8">
+									<div class="radio">
+										<label>
+											<input type="radio" name="optionsRadios" id="" value="">
+											Intérieur
+										</label>
+										<label>
+											<input type="radio" name="optionsRadios" id="" value="">
+											Extérieur
+										</label>
+									</div>
+								</div>
+							</div>
+							<div class="row info-terrain">
+								<div class="col-md-2">
+									<label for="eclairage" class="control-label"><b>Eclairage</b></label>
+								</div>
+								<div class="col-md-8">
+									<div class="radio">
+										<label>
+											<input type="radio" name="optionsRadios" id="" value="">
+											Eclairé
+										</label>
+										<label>
+											<input type="radio" name="optionsRadios" id="" value="">
+											Non Eclairé
+										</label>
+									</div>
+								</div>
+							</div>
+							<input class="btn btn-success" type="submit" value="Ajouter">
+						</form>
 					</div>
-				</form>
+					<div class="col-md-4">
+						<div class="row info-terrain">
+							<div class="col-md-2">
+								{{-- <label for="adresse" class="control-label"><b>Adresse</b></label> --}}
+							</div>
+							<div class="col-md-6">
+								<input class="form-content__input form-content__input--log form-control" type="text" placeholder="adresse terrain" aria-label=".form-control-lg" id="adresse" name="adresse">
+							</div>
+						</div>
+						<div id="map" style="height: 400px;"></div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
