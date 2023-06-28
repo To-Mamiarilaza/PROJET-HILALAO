27480d6af9d55972fcc91f17b102da9ddff2584c
Tiavina Malalaniaina <tiavinaramia@gmail.com>
2023-06-28 13:38:29 +0300

    availability

--- app/Http/Controllers/FOU/TestController.php
+++ app/Http/Controllers/FOU/TestController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\FOU;
 
 use App\Http\Controllers\Controller;
+use App\Models\FOU\FieldDetailled;
 
 class TestController extends Controller
 {
     public function index()
     {
-        return view('FOU\calendar');
+        $field = FieldDetailled::sFindById(3);
+        foreach ($field->getAvailability() as $key) {
+            var_dump($key->getDay());
+            echo "<br>";
+        }
+        return view('FOU\empty');
     }
 }
--- app/Models/FOU/Availability.php
+++ app/Models/FOU/Availability.php
@@ -1,24 +1,53 @@
 <?php
 namespace App\Models\FOU;
+
+use App\Models\FOU\Unavailability;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Eloquent\Model;
 use DateTime;
 
 class Availability extends DateTimeFO{
     private $price;
+    private $unavailability;
 
     public function __contruct($day = null, $start_time = null, $end_time = null, $price = null) {
         parent::__construct($day, $start_time, $end_time);
         $this->setPrice($price);
     }
 
-    public static function findByIdField($id_field) {
-        $sql = 'SELECT day, start_time, end_time, id_field, price FROM v_availability_field_for_one_month WHERE id_field=%s';
-        $sql = sprintf($sql, $id_field);
+    public static function groupByDate($array_availability) {
+        $result = [];
+        for ($i=0; $i<count($array_availability); $i++) {
+            $is_in = false;
+            for ($j=0; $j<count($result); $j++) {
+                if($array_availability[$i]->getDay() == $result[$j][$i]->getDay()) {
+                    $result[$i][] = $array_availability[$i];
+                    $is_in = true;
+                    break;
+                }
+            }
+            if (!$is_in){
+                $result[] = array($array_availability[$i]);
+            }
+        }
+        return $result;
+    }
+
+
+    public static function findByIdField($id_field, $date = null) {
+        if($date == null) {
+            $sql = 'SELECT day, start_time, end_time, id_field, price FROM v_availability_field_for_one_month WHERE id_field=%s';
+            $sql = sprintf($sql, $id_field);
+        } else {
+            $sql = "SELECT day, start_time, end_time, id_field, price FROM v_availlability_field_for_one_month WHERE id_field=%s AND day='%s'";
+            $sql = sprintf($sql, $id_field, $date);
+        }
         $availabilitys_db = DB::select($sql);
         $res = [];
+        $index = 0;
         foreach ($availabilitys_db as $result) {
-            $res[] = Availability::settingDBResult($result);
+            $res[$index] = Availability::settingDBResult($result);
+            $res[$index]->setUnavailability(Unavailability::findByIdField($id_field, $res[$index]->getDay()->format('Y-m-d')));
+            $index++;
         }
         return $res;
     }
@@ -33,12 +62,17 @@ class Availability extends DateTimeFO{
     }
 
 
-
     public function getPrice() {
         return $this->price;
     }
     public function setPrice($values) {
         $this->price = $values;
+    }
+    public function getUnavailability() {
+        return $this->unavailability;
+    }
+    public function setUnavailability($values) {
+        $this->unavailability = $values;
     }
 }
 ?>
