f7b228d9c6cf379b9d2d5462677f2bb4fe6182f5
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2023-07-05 14:14:47 +0200

    Update profile-field.blade.php

--- resources/views/FOC/profile-field.blade.php
+++ resources/views/FOC/profile-field.blade.php
@@ -238,10 +238,13 @@
                                 @endisset
 
                                 <input type="hidden" name="idImage" />
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Valider</button>
+
                             </div>
                         </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Valider</button>
                     </div>
                 </div>
             </div>
